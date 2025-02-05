6fe22237dcf73e469212bb49f953bcfed9d92341
Noah Pope <npope@tutanota.com>
2025-02-05 09:08:33 -0600

    Initial Commit

---
+++ Milestone4-HangManTests/Milestone4_HangManTests.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone4_HangManTests.swift
+//  Milestone4-HangManTests
+//
+//  Created by Noah Pope on 2/5/25.
+//
+
+import Testing
+@testable import Milestone4_HangMan
+
+struct Milestone4_HangManTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
